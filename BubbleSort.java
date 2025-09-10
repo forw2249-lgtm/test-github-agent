441e9cc358b46c7c7ac954a95255b1132d668bcb
forw2249-lgtm <forw2249@gmail.com>
2025-09-10 11:11:48 +0300

    Add BubbleSort.java - AI generated

--- BubbleSort.java
+++ BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
 
     public static void bubbleSort(int[] array) {
         int n = array.length;
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     // Swap array[j] and array[j+1]
